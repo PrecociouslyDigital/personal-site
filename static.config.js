699bded3fe296ba80c30d2ff768fccd4db757961
skyedelaciel <fellfromatree@gmail.com>
2019-10-29 16:01:03 -0700

    Add MDX Support

--- static.config.js
+++ static.config.js
@@ -1,38 +1,33 @@
-import axios from 'axios'
 import path from 'path'
-// import { Post } from './types'
-
-// Typescript support in static.config.js is not yet supported, but is coming in a future update!
-
+const find = require('find');
 export default {
   entry: path.join(__dirname, 'src', 'index.tsx'),
-  /*getRoutes: async () => {
-    return [
-      {
-        path: '/blog',
-        getData: () => ({
-          posts,
-        }),
-        children: posts.map((post) => ({
-          path: `/post/${post.id}`,
-          template: 'src/containers/Post',
-          getData: () => ({
-            post,
-          }),
-        })),
-      },
-    ]
-  },*/
+  getRoutes: async () => {
+    return [];
+  },
   plugins: [
+    [
+      require.resolve('react-static-plugin-mdx'),
+      {
+        includePaths: [], // Additional include paths on top of the default jsLoader paths
+        extensions: ['.md', '.mdx'], // NOTE: these are the default extensions
+        mdxOptions: {
+          remarkPlugins: [/* ... */],
+          rehypePlugins: [/* ... */],
+        },
+      },
+    ],
     'react-static-plugin-typescript',
     [
       require.resolve('react-static-plugin-source-filesystem'),
       {
         location: path.resolve('./src/pages'),
+        extensions: ["tsx", "mdx"],
       },
     ],
     require.resolve('react-static-plugin-reach-router'),
     require.resolve('react-static-plugin-sitemap'),
+
   ],
   devServer: {
     port: 8080,
